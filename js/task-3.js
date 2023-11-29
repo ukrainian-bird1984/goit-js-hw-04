f28cf08c5079559df3860a96c2c545b6569bbd40
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2023-11-30 00:43:09 +0100

    орп

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,21 @@
-function filterArray(numbers, value) {
-  const resultArray = [];
-
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      resultArray.push(numbers[i]);
-    }
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername: function(newName) {
+    this.username = newName;
+  },
+  updatePlayTime: function(hours) {
+    this.playTime += hours;
+  },
+  getInfo: function() {
+    return `${this.username} has ${this.playTime} active hours!`;
   }
+};
 
-  return resultArray;
-}
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
